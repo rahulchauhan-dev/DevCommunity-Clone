164c8cb913ec2f22a01485b001079cf8b70b47da
rahul <rahul.ion12@gmail.com>
2022-03-28 14:48:55 +0530

    footer done

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,14 +1,22 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Stack } from "react-bootstrap";
 
 const Footer = () => {
   return (
     <Container
       fluid
       className="d-flex justify-content-center pt-3"
-      style={{ borderTop: "1px solid lightgrey" }}
+      style={{
+        borderTop: "1px solid lightgrey",
+      }}
     >
-      <p>Copyright ©️ HowTo Community 2022.</p>
+      <Stack className="d-flex justify-content-center align-items-center">
+        <p>
+          <strong>HowTo Community</strong> — A constructive and inclusive social
+          network for software developers. With you every step of your journey.
+        </p>
+        <p>Made with love and MERN Stack. HowTo Community © 2022.</p>
+      </Stack>
     </Container>
   );
 };
